b7020ea09092ad8797e0a92d13a92fd6cc29c23f
jorge.marey <jorge.marey@bq.com>
2015-12-15 12:21:28 +0100

    Included version

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ import (
 const (
 	storePath     = "sensedns/network"
 	inventoryPath = "sensedns/inventory"
+	version       = "v0.1.0"
 )
 
 func getOrElse(env, def string) string {
@@ -33,6 +34,7 @@ func getNodeID(client *docker.Client) string {
 }
 
 func main() {
+	log.Printf("Starting SenseDNS %s\n", version)
 	dnsServer := &Server{
 		port:     53,
 		rTimeout: 5 * time.Second,
